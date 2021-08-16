d182718056e3b5a3ad78918b4a86501ceaddb241
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2021-08-15 21:18:57 -0500

    Added character list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
 import './App.scss';
-import axios from 'axios';
-import config from './config';
+import CharacterList from "./components/CharacterList";
 
 function App() {
-  axios.get(`${config.api.host}/hp-characters`)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CharacterList/>
     </div>
   );
 }
--- src/redux/index.js
+++ src/redux/index.js
@@ -5,4 +5,4 @@ const store = combineReducers({
     characterState: characterReducer
 });
 
-export default createStore(store);
+export default createStore(store, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
