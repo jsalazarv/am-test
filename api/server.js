537aa61cb9be9eed013ffe7e0fb0259d9357fa81
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2021-08-20 22:56:22 -0500

    add no cors in json server

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 var jsonServer  = require('json-server')
 var server      = jsonServer.create()
 var router      = jsonServer.router(require('./db.js')())
-var middlewares = jsonServer.defaults()
+var middlewares = jsonServer.defaults({ noCors: true })
 
 server.use(middlewares)
 server.use(router)
